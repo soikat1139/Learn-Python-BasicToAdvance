57e8e556154ade1338ae68a01a11d574cf703784
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2024-04-20 22:15:23 +0600

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -2,3 +2,16 @@
 str1="Hello"
 str2="World"
 print(str1,str2)
+#Tuple is very much similiar to list the main difference is unlike list you cannot change the elements of tuple once it is set
+
+aTuple=(1,3,5)
+#Tuple of Different data Types
+tupleData=("aTuple",1,2,3)
+
+#Nested Tuples
+
+nestedTuple=(1,"s",[5,4,3],[1,2])
+#Tuple can be accesed very much like a list at least
+
+
+
