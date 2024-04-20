9b6804117f0d3d1f08ae37d669f768bd99df32ba
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2024-04-20 23:02:44 +0600

    Create module.py

---
+++ module.py
@@ -0,0 +1,28 @@
+calnum.py
+print(cal num module)
+def add(a, b):
+result a+b
+return result
+ def subt(a, b):
+   result=a-b
+ return result
+ def mult(a, b):
+   result a b
+ return result
+
+ def div (a,b):
+ result a/b
+return result
+
+import cainum
+print(calnum.add(82,9.5))
+print(calnum.subt(18,4))
+ print(calnum.mult(5,8))
+
+ print(calnum.div(100,2))
+
+import calnum as cnum.
+print(cnum.div(4, 12.6))
+
+from calnum import div as dnum  
+dnum(500, 100)
