5435c0508c19ce429894e65390ab06de1179ebda
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2024-04-21 10:05:10 +0600

    Create ErrorHandling.py

---
+++ ErrorHandling.py
@@ -0,0 +1,3 @@
+Exception Handling
+
+There are going to be times when your code tries to show up some errors or other problem inside the code that you are doing, and this is where the exception is going to occur.
