05c83667807769ae6fa777c417082ea2353d53ca
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2024-04-21 10:07:27 +0600

    Update ErrorHandling.py

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -1,3 +1,15 @@
 Exception Handling
 
 There are going to be times when your code tries to show up some errors or other problem inside the code that you are doing, and this is where the exception is going to occur.
+
+Knowing when to recognize these exceptions, how to handle them, and even how to make some of your own can make a big difference in how well you are able to control your own coding in Python.
+A good example of an exception that your compiler is automatically going to raise up is when you or the user is to divide it by zero. The compiler is then going to recognize that this is something the user is not able to do, and it is going to send out that exception as an alert.
+It can also be something that is going to be called up if you, as the programmer, are trying to call up a function and the name is not spelled in the proper manner so there is no match present to bring up.
+
+If you are going through some of the code writing, and you start to notice that an exception will be raised, know that often the solution is going to be a simple one. But as the programmer, you need to take the time to get this fixed
+
+Built-in exceptions:
+Finally-this is the action that you will want to use to perform cleanup actions, whether the exceptions occur or not. I
+Assert this condition is going to trigger the exception inside of the code.
+Raise the raise command is going to trigger an exception manually inside of the code.
+• Try/except-this is when you want to try out a block of code and then it is recovered
